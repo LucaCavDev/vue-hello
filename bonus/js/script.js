a49c216776acbf34d26a52a9a71326784aaba715
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-16 16:11:25 +0100

    bonus1
    aggiunta immagine tramite data

---
+++ bonus/js/script.js
@@ -0,0 +1,14 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Hello Duck',
+    image : './img/duck.jpg'
+  }
+});
+
+// Altro possibile Bonus:
+// gestire una funzione con un evento nel modo Vue.
