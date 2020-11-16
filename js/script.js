f46d50d4a36eada624e617551f7a76e7d574fdc7
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-16 15:51:46 +0100

    completato

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Hello World'
+  }
+});
+
+
+
+
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+// Altro possibile Bonus:
+// gestire una funzione con un evento nel modo Vue.
